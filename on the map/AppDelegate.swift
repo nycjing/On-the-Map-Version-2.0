fb1c492e28c605362eb09c6ec4284782659391c8
Jing Jia <nycjing@gmail.com>
2015-09-01 12:32:37 -0400

    add login view control & Config & AppDelegate
    Need to revise everything reflected Udacity

--- on the map/AppDelegate.swift
+++ on the map/AppDelegate.swift
@@ -14,6 +14,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+        /* Constants for Udacity*/
+        let apiKey = "ENTER_YOUR_API_KEY_HERE"
+        let baseURLString = "http://api.themoviedb.org/3/"
+        let baseURLSecureString = "https://api.themoviedb.org/3/"
+        
+        /* Need these for login */
+        var requestToken: String? = nil
+        var sessionID: String? = nil
+        var userID: Int? = nil
+        
+        /* Configuration for TheMovieDB, we'll take care of this for you =)... */
+        var config = Config()
+        
+        func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+            
+            /* If necessary, update the configuration */
+            config.updateIfDaysSinceUpdateExceeds(7)
+            
+            return true
+        }
+    }
+    
+    // MARK: - Helper
+    
+    extension AppDelegate {
+        
+        /* Helper function: Given a dictionary of parameters, convert to a string for a url */
+        func escapedParameters(parameters: [String : AnyObject]) -> String {
+            
+            var urlVars = [String]()
+            
+            for (key, value) in parameters {
+                
+                /* Make sure that it is a string value */
+                let stringValue = "\(value)"
+                
+                /* Escape it */
+                let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+                
+                /* Append it */
+                urlVars += [key + "=" + "\(escapedValue!)"]
+                
+            }
+            
+            return (!urlVars.isEmpty ? "?" : "") + join("&", urlVars)
+        }
+        
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
@@ -38,74 +86,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
-    }
 
-    // MARK: - Core Data stack
-
-    lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "microbiz.com.on_the_map" in the application's documents Application Support directory.
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1] as! NSURL
-    }()
-
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("on_the_map", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
-    }()
-
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
-        // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("on_the_map.sqlite")
-        var error: NSError? = nil
-        var failureReason = "There was an error creating or loading the application's saved data."
-        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
-            coordinator = nil
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
-            dict[NSUnderlyingErrorKey] = error
-            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(error), \(error!.userInfo)")
-            abort()
-        }
-        
-        return coordinator
-    }()
-
-    lazy var managedObjectContext: NSManagedObjectContext? = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        if coordinator == nil {
-            return nil
-        }
-        var managedObjectContext = NSManagedObjectContext()
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        if let moc = self.managedObjectContext {
-            var error: NSError? = nil
-            if moc.hasChanges && !moc.save(&error) {
-                // Replace this implementation with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                NSLog("Unresolved error \(error), \(error!.userInfo)")
-                abort()
-            }
-        }
-    }
-
-}
 
---
+++ on the map/LoginViewController.swift
@@ -0,0 +1,402 @@
+//
+//  LoginViewController.swift
+//  on the map
+//
+//  Created by Jing Jia on 9/1/15.
+//  Copyright (c) 2015 Jing Jia. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var headerTextLabel: UILabel!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: LoginButton!
+    @IBOutlet weak var debugTextLabel: UILabel!
+    
+    var appDelegate: AppDelegate!
+    var session: NSURLSession!
+    
+    var backgroundGradient: CAGradientLayer? = nil
+    var tapRecognizer: UITapGestureRecognizer? = nil
+    
+    /* Based on student comments, this was added to help with smaller resolution devices */
+    var keyboardAdjusted = false
+    var lastKeyboardOffset : CGFloat = 0.0
+    
+    // MARK: - Initialization
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Get the app delegate */
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        /* Get the shared URL session */
+        session = NSURLSession.sharedSession()
+        
+        /* Configure the UI */
+        self.configureUI()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.addKeyboardDismissRecognizer()
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.removeKeyboardDismissRecognizer()
+        self.unsubscribeToKeyboardNotifications()
+    }
+    
+    // MARK: - Keyboard Fixes
+    
+    func addKeyboardDismissRecognizer() {
+        self.view.addGestureRecognizer(tapRecognizer!)
+    }
+    
+    func removeKeyboardDismissRecognizer() {
+        self.view.removeGestureRecognizer(tapRecognizer!)
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - Login
+    
+    @IBAction func loginButtonTouch(sender: AnyObject) {
+        if usernameTextField.text.isEmpty {
+            debugTextLabel.text = "Username Empty."
+        } else if passwordTextField.text.isEmpty {
+            debugTextLabel.text = "Password Empty."
+        } else {
+            
+            /*
+            Steps for Authentication...
+            https://www.themoviedb.org/documentation/api/sessions
+            
+            Step 1: Create a new request token
+            Step 2: Ask the user for permission via the API ("login")
+            Step 3: Create a session ID
+            
+            Extra Steps...
+            Step 4: Go ahead and get the user id ;)
+            Step 5: Got everything we need, go to the next view!
+            
+            */
+            self.getRequestToken()
+        }
+    }
+    
+    func completeLogin() {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.debugTextLabel.text = ""
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("MoviesTabBarController") as! UITabBarController
+            self.presentViewController(controller, animated: true, completion: nil)
+        })
+    }
+    
+    // MARK: - Service Calls
+    
+    func getRequestToken() {
+        
+        /* TASK: Get a request token, then store it (appDelegate.requestToken) and login with the token */
+        
+        /* 1. Set the parameters */
+        let methodParameters = [
+            "api_key": appDelegate.apiKey
+        ]
+        
+        /* 2. Build the URL */
+        let urlString = appDelegate.baseURLSecureString + "authentication/token/new" + appDelegate.escapedParameters(methodParameters)
+        let url = NSURL(string: urlString)!
+        
+        /* 3. Configure the request */
+        let request = NSMutableURLRequest(URL: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        /* 4. Make the request */
+        let task = session.dataTaskWithRequest(request) { data, response, downloadError in
+            
+            if let error = downloadError {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.debugTextLabel.text = "Login Failed (Request Token)."
+                }
+                println("Could not complete the request \(error)")
+            } else {
+                
+                /* 5. Parse the data */
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
+                
+                /* 6. Use the data! */
+                if let requestToken = parsedResult["request_token"] as? String {
+                    self.appDelegate.requestToken = requestToken
+                    self.loginWithToken(self.appDelegate.requestToken!)
+                } else {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.debugTextLabel.text = "Login Failed (Request Token)."
+                    }
+                    println("Could not find request_token in \(parsedResult)")
+                }
+            }
+        }
+        
+        /* 7. Start the request */
+        task.resume()
+    }
+    
+    func loginWithToken(requestToken: String) {
+        
+        /* TASK: Login, then get a session id */
+        
+        /* 1. Set the parameters */
+        let methodParameters = [
+            "api_key": appDelegate.apiKey,
+            "request_token": requestToken,
+            "username": self.usernameTextField.text,
+            "password": self.passwordTextField.text
+        ]
+        
+        /* 2. Build the URL */
+        let urlString = appDelegate.baseURLSecureString + "authentication/token/validate_with_login" + appDelegate.escapedParameters(methodParameters)
+        let url = NSURL(string: urlString)!
+        
+        /* 3. Configure the request */
+        let request = NSMutableURLRequest(URL: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        /* 4. Make the request */
+        let task = session.dataTaskWithRequest(request) { data, response, downloadError in
+            
+            if let error = downloadError {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.debugTextLabel.text = "Login Failed (Login Step)."
+                }
+                println("Could not complete the request \(error)")
+            } else {
+                
+                /* 5. Parse the data */
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
+                
+                /* 6. Use the data! */
+                if let success = parsedResult["success"] as? Bool {
+                    if success {
+                        self.getSessionID(self.appDelegate.requestToken!)
+                    } else {
+                        println("Login failed.")
+                    }
+                } else {
+                    if let status_code = parsedResult["status_code"] as? Int {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            self.debugTextLabel.text = parsedResult["status_message"] as? String
+                        }
+                    } else {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            self.debugTextLabel.text = "Login Failed (Login Step)."
+                        }
+                        println("Could not find status_code in \(parsedResult)")
+                    }
+                }
+            }
+        }
+        
+        /* 7. Start the request */
+        task.resume()
+    }
+    
+    func getSessionID(requestToken: String) {
+        
+        /* 1. Set the parameters */
+        let methodParameters = [
+            "api_key": appDelegate.apiKey,
+            "request_token": requestToken
+        ]
+        
+        /* 2. Build the URL */
+        let urlString = appDelegate.baseURLSecureString + "authentication/session/new" + appDelegate.escapedParameters(methodParameters)
+        let url = NSURL(string: urlString)!
+        
+        /* 3. Configure the request */
+        let request = NSMutableURLRequest(URL: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        /* 4. Make the request */
+        let task = session.dataTaskWithRequest(request) { data, response, downloadError in
+            
+            if let error = downloadError {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.debugTextLabel.text = "Login Failed (Session ID)."
+                }
+                println("Could not complete the request \(error)")
+            } else {
+                
+                /* 5. Parse the data */
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
+                
+                /* 6. Use the data! */
+                if let sessionID = parsedResult["session_id"] as? String {
+                    self.appDelegate.sessionID = sessionID
+                    self.getUserID(self.appDelegate.sessionID!)
+                } else {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.debugTextLabel.text = "Login Failed (Session ID)."
+                    }
+                    println("Could not find session_id in \(parsedResult)")
+                }
+            }
+        }
+        
+        /* 7. Start the request */
+        task.resume()
+    }
+    
+    func getUserID(session_id : String) {
+        
+        /* TASK: Get the user's ID, then store it (appDelegate.userID) for future use and go to next view! */
+        
+        /* 1. Set the parameters */
+        let methodParameters = [
+            "api_key": appDelegate.apiKey,
+            "session_id": session_id
+        ]
+        
+        /* 2. Build the URL */
+        let urlString = appDelegate.baseURLSecureString + "account" + appDelegate.escapedParameters(methodParameters)
+        let url = NSURL(string: urlString)!
+        
+        /* 3. Configure the request */
+        let request = NSMutableURLRequest(URL: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        /* 4. Make the request */
+        let task = session.dataTaskWithRequest(request) { data, response, downloadError in
+            
+            if let error = downloadError {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.debugTextLabel.text = "Login Failed (User ID)."
+                }
+                println("Could not complete the request \(error)")
+            } else {
+                
+                /* 5. Parse the data */
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
+                
+                /* 6. Use the data! */
+                if let userID = parsedResult["id"] as? Int {
+                    self.appDelegate.userID = userID
+                    self.completeLogin()
+                    
+                } else {
+                    dispatch_async(dispatch_get_main_queue()) {
+                        self.debugTextLabel.text = "Login Failed (User ID)."
+                    }
+                    println("Could not find id in \(parsedResult)")
+                }
+            }
+        }
+        
+        /* 7. Start the request */
+        task.resume()
+    }
+}
+
+// MARK: - Helper
+
+extension LoginViewController {
+    
+    func configureUI() {
+        
+        /* Configure background gradient */
+        self.view.backgroundColor = UIColor.clearColor()
+        let colorTop = UIColor(red: 0.345, green: 0.839, blue: 0.988, alpha: 1.0).CGColor
+        let colorBottom = UIColor(red: 0.023, green: 0.569, blue: 0.910, alpha: 1.0).CGColor
+        self.backgroundGradient = CAGradientLayer()
+        self.backgroundGradient!.colors = [colorTop, colorBottom]
+        self.backgroundGradient!.locations = [0.0, 1.0]
+        self.backgroundGradient!.frame = view.frame
+        self.view.layer.insertSublayer(self.backgroundGradient, atIndex: 0)
+        
+        /* Configure header text label */
+        headerTextLabel.font = UIFont(name: "AvenirNext-Medium", size: 24.0)
+        headerTextLabel.textColor = UIColor.whiteColor()
+        
+        /* Configure email textfield */
+        let emailTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let emailTextFieldPaddingView = UIView(frame: emailTextFieldPaddingViewFrame)
+        usernameTextField.leftView = emailTextFieldPaddingView
+        usernameTextField.leftViewMode = .Always
+        usernameTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        usernameTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        usernameTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        usernameTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        /* Configure password textfield */
+        let passwordTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let passwordTextFieldPaddingView = UIView(frame: passwordTextFieldPaddingViewFrame)
+        passwordTextField.leftView = passwordTextFieldPaddingView
+        passwordTextField.leftViewMode = .Always
+        passwordTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        passwordTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        passwordTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        passwordTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        /* Configure debug text label */
+        headerTextLabel.font = UIFont(name: "AvenirNext-Medium", size: 20)
+        headerTextLabel.textColor = UIColor.whiteColor()
+        
+        /* Configure tap recognizer */
+        tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        tapRecognizer?.numberOfTapsRequired = 1
+        
+    }
+}
+
+/* This code has been added in response to student comments */
+extension LoginViewController {
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if keyboardAdjusted == false {
+            lastKeyboardOffset = getKeyboardHeight(notification) / 2
+            self.view.superview?.frame.origin.y -= lastKeyboardOffset
+            keyboardAdjusted = true
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        
+        if keyboardAdjusted == true {
+            self.view.superview?.frame.origin.y += lastKeyboardOffset
+            keyboardAdjusted = false
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+}
+
