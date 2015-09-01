3ba68ba82175e20442b1601d6c646bd7c213db53
Jing Jia <nycjing@gmail.com>
2015-09-01 11:54:17 -0400

    add login button
    add login button

---
+++ on the map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  on the map
+//
+//  Created by Jing Jia on 9/1/15.
+//  Copyright (c) 2015 Jing Jia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
