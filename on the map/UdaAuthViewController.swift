8f9b653c9f3cd6c5c586e189ee0a8d6233f5e883
Jing Jia <nycjing@gmail.com>
2015-09-19 08:50:33 -0400

    try to get login button work

--- on the map/UdaAuthViewController.swift
+++ on the map/UdaAuthViewController.swift
@@ -47,6 +47,22 @@ class UdaAuthViewController: UIViewController, UIWebViewDelegate {
                 self.completionHandler!(success: true, errorString: nil)
             })
         }
+       /*
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"nycjing@gmail.com\", \"password\": \"Spring2014\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+            println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
+*/
     }
     
     func cancelAuth() {
